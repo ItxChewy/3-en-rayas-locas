18015ff8cc1b3d70c98f3e26ded5440337aab29a
ItxChewy <aarongarcia1906@gmail.com>
2023-11-17 10:14:29 +0100

    Juego bueno

--- juego.js
+++ juego.js
@@ -1,13 +1,9 @@
-let cuerpo;
+let cuerpo; 
 let caja_boton;
-let turnoAzul = false;
-let turnoNaranja = false;
-
 window.addEventListener("load", inicio);
-
 function allowDrop(ev) {
     ev.preventDefault();
-    console.log("DragOver");
+    console.log("DragOver")
 }
 
 function drag(ev) {
@@ -25,54 +21,92 @@ function inicio() {
     var fichasAzules = document.getElementsByClassName("ficha-azul");
     var fichasNaranjas = document.getElementsByClassName("ficha-naranja");
     var numeroAleatorio = Math.round(Math.random());
+    var turnoAzul = false;
+    var turnoNaranja = false;
     const datoTiempo = document.getElementById("tiempo");
     const tiempoParseado = parseInt(datoTiempo.textContent);
-    
-    // Llamada inicial a turnos para establecer el primer turno y draggable
-    turnos();
-
     document.getElementById("btn-frenesi").addEventListener("click", modoFrenesi);
-
-    function quitarDrag(fichas, turno) {
-        for (let i = 0; i < 9; i++) {
-            fichas[i].setAttribute("draggable", turno ? "true" : "false");
-        }
-    }
-
-    function turnos() {
-        if (numeroAleatorio === 0) {
+   /* function turnoAleatorio(){
+        
+        console.log(numeroAleatorio);
+        if (numeroAleatorio == 0){
             turnoAzul = true;
-        } else {
+        }else{
             turnoNaranja = true;
         }
+    }*/
+    console.log(numeroAleatorio);
+    function quitarDrag(fichasAzules, fichasNaranjas){
+        if(!turnoAzul){
+            for(let i =0;i<9;i++){
+                fichasAzules[i].setAttribute("draggable","false");
+            }
+        }else{
+            for(let i =0;i<9;i++){
+                fichasAzules[i].setAttribute("draggable","true");
+            }
+        }
 
-        quitarDrag(fichasAzules, turnoAzul);
-        quitarDrag(fichasNaranjas, turnoNaranja);
-        document.getElementById("turno").textContent = numeroAleatorio;
+        if(!turnoNaranja){
+            for(let i =0;i<9;i++){
+                fichasNaranjas[i].setAttribute("draggable","false");
+            }
+        }else{
+            for(let i =0;i<9;i++){
+                fichasNaranjas[i].setAttribute("draggable","true");
+            }
+        }
+    
+    }
+   quitarDrag(fichasAzules,fichasNaranjas);
+ 
+   function turnos(){
+    var turno = numeroAleatorio;
+    if (numeroAleatorio == 0){
+        turnoAzul = true;
+        turno++;
+    }else{
+        turnoNaranja = true;
+        turno++;
     }
 
-    function temporizador(tiempoParseado) {
-        setInterval(() => {
-            if (tiempoParseado < 0) {
-                tiempoParseado = 15;
-            }
-            if (tiempoParseado <= 5) {
-                datoTiempo.style.color = 'red';
-                datoTiempo.style.fontWeight = 'bold';
-            } else {
-                datoTiempo.style.color = 'black';
-                datoTiempo.style.fontWeight = 'normal';
-            }
-            datoTiempo.textContent = tiempoParseado;
-            tiempoParseado--;
-        }, 1000);
+    if(turno%2 == 0){
+        turnoAzul = true;
+        turnoNaranja = false;
+        quitarDrag(fichasAzules,fichasNaranjas);
+        document.getElementById("turno").textContent = turno;
+        turno++;
+    }else{
+        turnoAzul = false;
+        turnoNaranja = true;
+        quitarDrag(fichasAzules,fichasNaranjas); 
+        turno++;
     }
 
-    temporizador(tiempoParseado);
+   }
+    function temporizador(tiempoParseado){
+        setInterval(()=> {
+                if(tiempoParseado < 0){
+                    tiempoParseado = 15;
+                }
+                if(tiempoParseado <= 5){
+                    datoTiempo.style.color = 'red';
+                    datoTiempo.style.fontWeight = 'bold';
+                } else {
+                    datoTiempo.style.color = 'black';
+                    datoTiempo.style.fontWeight = 'normal';
+                }
+                datoTiempo.textContent = tiempoParseado
+                tiempoParseado--;
+            },1000);
+    }
+
+        temporizador(tiempoParseado);
 }
 
 function modoFrenesi() {
     cuerpo.classList.add("animacionBody");  
-    caja_boton[0].style.display = "none";
+    caja_boton[0].style.display="none";
 }
+
 
