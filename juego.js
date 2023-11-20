dd3a92a72856390d57fa383e7b777791cc78e3e8
ItxChewy <aarongarcia1906@gmail.com>
2023-11-20 10:24:42 +0100

    El bueno

--- juego.js
+++ juego.js
@@ -15,24 +15,83 @@ function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
-function drop(ev) {
-    let fichaPuesta = true;
 
-    var data = ev.dataTransfer.getData("text");
+let tiempoReseteado = false;
 
-     if((data.charAt(0) == 'a') && turnoActual%2==0){
-         ev.target.appendChild(document.getElementById(data));
-         fichaPuesta = false;
-     }
-     if((data.charAt(0) == 'n') && turnoActual%2==1){
-         ev.target.appendChild(document.getElementById(data));
-         fichaPuesta = true;
-     }
+function comprobarFichas(data, ev){
+  switch(data[data.length-1]){
+    case 'x':
+      if(!ev.target.hasChildNodes()){
+        ev.target.appendChild(document.getElementById(data));
+        //se mete ficha
+      } else {
+        if(ev.target.firstChild.classList.contains('l') || ev.target.firstChild.classList.contains('s')){
+          ev.target.removeChild(ev.target.firstChild);
+          ev.target.appendChild(document.getElementById(data));
+        } else {
+          alert("No puedes poner aqui")
+        }
+      }
+      console.log(data);
+      break;
+    case 'l':
+      if(!ev.target.hasChildNodes()){
+        ev.target.appendChild(document.getElementById(data));
+        //se mete ficha
+      } else {
+        if(ev.target.firstChild.classList.contains('s')){
+          ev.target.removeChild(ev.target.firstChild);
+          ev.target.appendChild(document.getElementById(data));
+        } else {
+          alert("No puedes poner aqui")
+        }
+      }
+      break;
+    case 's':
+      if(!ev.target.hasChildNodes()){
+        ev.target.appendChild(document.getElementById(data));
+        //se mete ficha
+      }
 
-    console.log(data.charAt(0))
-
+      console.log(data);
+      break;
+  }
 }
 
+function drop(ev) {
+    //Data es la ficha que cogemos (id) --> la bolita
+    var data = ev.dataTransfer.getData("text");
+    // console.log(ev.target)
+    console.log(data[data.length-1])
+  
+    if(ev.target.classList.contains('casilla-tablero')){
+      // if(!ev.target.hasChildNodes()){
+        if((data.charAt(0) == 'a') && turnoActual%2==0){
+         comprobarFichas(data, ev);
+          // ev.target.appendChild(document.getElementById(data));
+          tiempoReseteado = true;
+          console.log("El primer hijo es: "+ev.target.firstChild.id)
+          console.log("Cambia de turno en azul")
+  
+      }
+      if((data.charAt(0) == 'n') && turnoActual%2==1){
+        comprobarFichas(data, ev);
+          tiempoReseteado = true;
+          console.log("El primer hijo es: "+ev.target.firstChild.id)
+  
+          console.log("Cambia de turno en naranja")
+  
+      }
+      // }
+    }
+     
+     console.log(tiempoReseteado)
+
+    
+
+    // console.log(data.charAt(0))
+
+}
 function turnos() {
   if (turnoActual % 2 != 0) {
     document.getElementById("turno").textContent = "AZUL";
@@ -49,22 +108,25 @@ function inicio() {
   caja_boton = document.getElementsByClassName("cont-frenesi")[0];
   fichasAzules = document.getElementsByClassName("ficha-azul");
   fichasNaranjas = document.getElementsByClassName("ficha-naranja");
-  datoTiempo = document.getElementById("tiempo"); // Inicializada como variable global
-
+  datoTiempo = document.getElementById("tiempo"); 
   const tiempoParseado = parseInt(datoTiempo.textContent);
   document.getElementById("btn-frenesi").addEventListener("click", activarModoFrenesi);
 
   function temporizador(tiempoParseado) {
       setInterval(() => {
-        if (tiempoParseado < 0) {
+        if (tiempoParseado < 0 || tiempoReseteado) {
+          tiempoReseteado = false;
+          console.log(tiempoReseteado)
           turnos();
-          if(click== 0){
+          console.log("Cambia de turno en temporizador")
+          if(click== 0 ){
             tiempoParseado = 10;
           }else{
             tiempoParseado = 5;
-          }
-        
+          }  
+          
         }
+
         if (tiempoParseado <= 5) {
           datoTiempo.style.color = "red";
           datoTiempo.style.fontWeight = "bold";
@@ -74,8 +136,9 @@ function inicio() {
         }
         datoTiempo.textContent = tiempoParseado;
         tiempoParseado--;
+      
       }, 1000);
-    
+
   }
 temporizador(tiempoParseado);
  
@@ -88,4 +151,3 @@ function activarModoFrenesi() {
   caja_boton.style.display = "none";
   click++;
 }
-
