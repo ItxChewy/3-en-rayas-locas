a4eabf320cdb7e1443884c588b4b63b29ae7213a
ItxChewy <aarongarcia1906@gmail.com>
2023-11-29 16:47:46 +0100

    Casi terminado

--- juego.js
+++ juego.js
@@ -13,8 +13,6 @@ const casillaTablero = document.getElementsByClassName("casilla-tablero");
 let partidaAcabada = false;
 let ranking;
 
-
-
 window.addEventListener("load", inicio);
 
 function inicio() {
@@ -37,35 +35,41 @@ function inicio() {
   document.getElementById("inicio").addEventListener("click", volverInicio);
   document.getElementById("revancha").addEventListener("click", jugarRevancha);
   function temporizador(tiempoParseado) {
-    setInterval(() => {
-      if(!partidaAcabada){
-        if (tiempoParseado < 0 || tiempoReseteado) {
-          tiempoReseteado = false;
-          console.log(tiempoParseado)
-          turnos();
-          console.log("Cambia de turno en temporizador")
-          if(click== 0 ){
-            tiempoParseado = 10;
-          }else{
-            tiempoParseado = 5; 
-          }  
+  
+      setInterval(() => {
+        if(!partidaAcabada){
+          if (tiempoParseado < 0 || tiempoReseteado) {
+            tiempoReseteado = false;
+            console.log(tiempoParseado)
+            turnos();
+            console.log("Cambia de turno en temporizador")
+            if(click== 0 ){
+              tiempoParseado = 10;
+            }else{
+              tiempoParseado = 5;
+              
+            }  
+            
+          }
+          if (tiempoParseado <= 5) {
+            datoTiempo.style.color = "red";
+            datoTiempo.style.fontWeight = "bold";
+              if(tiempoParseado <= 3){ 
+                alarmaReloj.play();
+              }
+            
+          } else {
+            datoTiempo.style.color = "black";
+            datoTiempo.style.fontWeight = "normal";
+            alarmaReloj.pause();
+          }
+          datoTiempo.textContent = tiempoParseado;
+          tiempoParseado--;
         }
-        if (tiempoParseado <= 5) {
-          datoTiempo.style.color = "red";
-          datoTiempo.style.fontWeight = "bold";
-            if(tiempoParseado <= 3){ 
-              alarmaReloj.play();
-            }
-        } else {
-          datoTiempo.style.color = "black";
-          datoTiempo.style.fontWeight = "normal";
-          alarmaReloj.pause();
-        }
-        datoTiempo.textContent = tiempoParseado;
-        console.log("tiempo parseado: " + tiempoParseado);
-        tiempoParseado--;
-      }
-    }, 1000);  
+       
+      }, 1000);
+    
+      
   }
 temporizador(tiempoParseado);
 }
@@ -87,7 +91,7 @@ function comprobarFichas(data, ev) {
         ficha.draggable = false; // Desactiva la capacidad de arrastre
       } else {
         if (ev.target.classList.contains('ficha-azul') || ev.target.classList.contains('ficha-naranja')) {
-          if (ev.target.classList.contains('s')) {
+          if (ev.target.classList.contains('l')) {
             let parent = ev.target.parentNode;
             parent.removeChild(parent.firstChild);
             parent.appendChild(document.getElementById(data));
@@ -140,7 +144,6 @@ function comprobarFichas(data, ev) {
   }
 }
 
-
 function drop(ev) {
     //Data es la ficha que cogemos (id) --> la bolita
     console.log(ev.target.classList)
@@ -184,20 +187,12 @@ function comprobaciones(){
 
 function comprobarTableroLleno(){
   var contador = 0;
-  var xl = 0;
-  var s = 0;
   for(let i =0;i<casillaTablero.length;i++){
       if(casillaTablero[i].hasChildNodes()){
         contador++;
       }
-      if(casillaTablero[i].firstChild.classList[1] == "xl"){
-        xl++;
-      }
-      if(casillaTablero[i].firstChild.classList[1] == "s"){
-        s++;
-      }
   }
-  if(contador==9 && xl==6 && s==0 ){
+  if(contador==9){
     alert("Tablero lleno");
     document.getElementById("alerta").classList.remove("inactiva");
   }else{
@@ -207,20 +202,29 @@ function comprobarTableroLleno(){
 
 
 function comprobarHorizontal() {
-  let victoria = false; 
+  let victoria = false;
+    
   for(let i = 0; i < casillaTablero.length; i = i + 3){
     if(casillaTablero[i].hasChildNodes() && casillaTablero[i+1].hasChildNodes() && casillaTablero[i+2].hasChildNodes()) {
       if ((casillaTablero[i].firstChild.classList.contains("ficha-azul") && casillaTablero[i+1].firstChild.classList.contains("ficha-azul") && casillaTablero[i+2].firstChild.classList.contains("ficha-azul")) 
       || (casillaTablero[i].firstChild.classList.contains("ficha-naranja") && casillaTablero[i+1].firstChild.classList.contains("ficha-naranja") && casillaTablero[i+2].firstChild.classList.contains("ficha-naranja"))){
         victoria = true;
+        if(casillaTablero[i].firstChild.classList.contains("ficha-azul")){
+          ganador = "azul";
+        } else {
+          ganador = "naranja"
+        }
       }
     }
   }
   if (victoria) {
     partidaAcabada = true;
+    sacarGanador(ganador);
+
     victoriaAlert.play();
-    alert("Victoria");
+   // alert("Victoria");
     document.getElementById("alerta").classList.remove("inactiva");
+    //document.getElementById("cont-juego").classList.add("inactiva");
   }
 }
 
@@ -231,36 +235,84 @@ function comprobarVertical(){
       if ((casillaTablero[i].firstChild.classList.contains("ficha-azul") && casillaTablero[i+3].firstChild.classList.contains("ficha-azul") && casillaTablero[i+6].firstChild.classList.contains("ficha-azul")) 
       || (casillaTablero[i].firstChild.classList.contains("ficha-naranja") && casillaTablero[i+3].firstChild.classList.contains("ficha-naranja") && casillaTablero[i+6].firstChild.classList.contains("ficha-naranja"))){
         victoria = true;
+        if(casillaTablero[i].firstChild.classList.contains("ficha-azul")){
+          ganador = "azul";
+        } else {
+          ganador = "naranja"
+        }
+
       }
     }
   }
   if (victoria) {
     partidaAcabada = true;
+    sacarGanador(ganador);
+
     victoriaAlert.play();
-    alert("Victoria");
+   //alert("Victoria");
     document.getElementById("alerta").classList.remove("inactiva");
+    //document.getElementById("cont-juego").classList.add("inactiva");
   }
 }
-
+let ganador = "";
+var arrayGanadores = [];
 function comprobarDiagonal(){
   let victoria = false;
+  
   if(casillaTablero[0].hasChildNodes() && casillaTablero[4].hasChildNodes() && casillaTablero[8].hasChildNodes()) {
     if ((casillaTablero[0].firstChild.classList.contains("ficha-azul") && casillaTablero[4].firstChild.classList.contains("ficha-azul") && casillaTablero[8].firstChild.classList.contains("ficha-azul")) || (casillaTablero[0].firstChild.classList.contains("ficha-naranja") && casillaTablero[4].firstChild.classList.contains("ficha-naranja") && casillaTablero[8].firstChild.classList.contains("ficha-naranja"))){
       victoria = true;
+      if(casillaTablero[0].firstChild.classList.contains("ficha-azul")){
+        ganador = "azul";
+      } else {
+        ganador = "naranja"
+      }
     }
   }
   if(casillaTablero[2].hasChildNodes() && casillaTablero[4].hasChildNodes() && casillaTablero[6].hasChildNodes()) {
     if ((casillaTablero[2].firstChild.classList.contains("ficha-azul") && casillaTablero[4].firstChild.classList.contains("ficha-azul") && casillaTablero[6].firstChild.classList.contains("ficha-azul")) || (casillaTablero[2].firstChild.classList.contains("ficha-naranja") && casillaTablero[4].firstChild.classList.contains("ficha-naranja") && casillaTablero[6].firstChild.classList.contains("ficha-naranja"))){
       victoria = true;
+      if(casillaTablero[2].firstChild.classList.contains("ficha-azul")){
+        ganador = "azul";
+      } else {
+        ganador = "naranja"
+      }
     }
   }
   if (victoria) {
     partidaAcabada = true;
+    sacarGanador(ganador);
     victoriaAlert.play();
-    alert("Victoria");
+    //alert("Victoria");
     document.getElementById("alerta").classList.remove("inactiva");
+    //document.getElementById("cont-juego").classList.add("inactiva");
   }
 }
+
+  function sacarGanador(ganador){
+    var strGanador = "";
+    if(ganador == "azul"){
+      var nombreGanador = document.createElement('p');
+      strGanador = localStorage.getItem('J1');
+      nombreGanador.textContent = strGanador;
+      arrayGanadores.push(nombreGanador)
+      // ranking.appendChild(nombreGanador)
+      //alert(`El ganador es ${localStorage.getItem('J1')}`)
+    } else {
+      var nombreGanador = document.createElement('p');
+      strGanador = localStorage.getItem('J2');
+      nombreGanador.textContent = strGanador;
+      arrayGanadores.push(nombreGanador)
+      // ranking.appendChild(nombreGanador);
+      //alert(`El ganador es ${localStorage.getItem('J2')}`)
+    }
+
+    arrayGanadores.forEach(element => {
+      ranking.appendChild(element);
+    });
+
+    console.log(arrayGanadores)
+  }
 
   function volverInicio(){
     window.location.href='index1-1.html';
@@ -270,13 +322,11 @@ function comprobarDiagonal(){
     location.reload();
   }
 
-function activarModoFrenesi() {
-  if(partidaAcabada == false){
-  cuerpo.classList.add("animacionBody");
-  risaFrenesi.play();
-  caja_boton.style.display = "none";
-  click++;
-}
-}
+  function activarModoFrenesi() {
+    cuerpo.classList.add("animacionBody");
+    risaFrenesi.play();
+    caja_boton.style.display = "none";
+    click++;
+  }
 
 
